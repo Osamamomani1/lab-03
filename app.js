0ae793c9c69bbb2932855ccc89bc16b80de6df25
osamamomani1 <>
2021-04-15 10:52:16 +0000

    lab05

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*var userName = prompt ('your name please');
 =======
 var userName = prompt ('your name please');
@@ -21,27 +20,27 @@ if(timeNow >= 18){
 }
 */
 
+var carType = prompt('Hello, please enter your fav car motor type!');
 
-
-var carType = prompt('Hello, please enter your car motor type ');
 
 while(carType !== 'engine'&& carType !== 'electric'){
-  carType = prompt('Please enter the name engine or electric');
+  carType = prompt('Please enter the name engine or electric ');
 }
+
 
 var userChoose;
 
 if(carType === 'engine'){
 userChoose='<img src="https://www.carscoops.com/wp-content/uploads/2018/12/5862586a-ferrari-enzo-00.jpg">';
-}else if (carType === 'electric'){
-  userChoose='<img src="https://i.pinimg.com/originals/21/2c/af/212caf9217de928c78d597b7f2bb4d52.jpg">';
+}
+else if (carType === 'electric'){
+  userChoose='<img src="https://spectrum.ieee.org/image/MzM5ODEzNg.jpeg">';
 }
 
-var imagesNumber= prompt('How many image do you want?');
+
+var imagesNumber= prompt('How many image do you want???');
 
 for(var i=0 ; i < imagesNumber; i++){
-  document.write(userChoose);
+  document.write (userChoose);
 }
-=======
-}
->>>>>>> a1c9bd23374a6bdae3595ffc684c847de65fe881
+
