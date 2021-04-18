838c1afc37f82551f80bb7763cdbc680ab97ee01
osamamomani1 <>
2021-04-18 10:52:57 +0000

    JS after CSS

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*var userName = prompt ('your name please');
 =======
 var userName = prompt ('your name please');
@@ -19,29 +18,30 @@ if(timeNow >= 18){
   document.write('<img src="https://img.huffingtonpost.com/asset/5e0f68ec2500003b1998fb2e.jpeg?cache=YqiWjN9UVt&ops=crop_34_446_5966_3406%2Cscalefit_720_noupscale">')
 <<<<<<< HEAD
 }
+______________________________---________________
 */
 
+var carType = prompt('Hello, please enter your fav car motor type!');
 
-
-var carType = prompt('Hello, please enter your car motor type ');
 
 while(carType !== 'engine'&& carType !== 'electric'){
-  carType = prompt('Please enter the name engine or electric');
+  carType = prompt('Please enter the name engine or electric ');
 }
+
 
 var userChoose;
 
 if(carType === 'engine'){
 userChoose='<img src="https://www.carscoops.com/wp-content/uploads/2018/12/5862586a-ferrari-enzo-00.jpg">';
-}else if (carType === 'electric'){
-  userChoose='<img src="https://i.pinimg.com/originals/21/2c/af/212caf9217de928c78d597b7f2bb4d52.jpg">';
+}
+else if (carType === 'electric'){
+  userChoose='<img src="https://spectrum.ieee.org/image/MzM5ODEzNg.jpeg">';
 }
 
-var imagesNumber= prompt('How many image do you want?');
+
+var imagesNumber= prompt('How many image do you want???');
 
 for(var i=0 ; i < imagesNumber; i++){
-  document.write(userChoose);
+  document.write (userChoose);
 }
-=======
-}
->>>>>>> a1c9bd23374a6bdae3595ffc684c847de65fe881
+
